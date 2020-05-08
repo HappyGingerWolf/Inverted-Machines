86e0427cf8b6de27c6bfebe9bedf1b7be3046ef6
MEEP of Faith <meepmatthew@gmail.com>
2020-05-08 04:03:40 -0700

    production limiting
    revert flamre color

--- scripts/inverted-silicon-smelter.js
+++ scripts/inverted-silicon-smelter.js
@@ -2,34 +2,8 @@ const invertedSiliconSmelter = extendContent(GenericSmelter, "inverted-silicon-s
   setStats(){
     this.super$setStats();
     
-    this.stats.add(BlockStat.output, this.outputItems[0]);
-    this.stats.add(BlockStat.output, this.outputItems[1]);
-  },
-  load(){
-    this.region = Core.atlas.find(this.name);
-    this.topRegion = Core.atlas.find(this.name + "-top");
-  },
-  draw(tile){
-    entity = tile.ent();
-    
-    Draw.rect(this.region, tile.drawx(), tile.drawy(), 0);
-    
-    //draw glowing center
-    if(entity.warmup > 0 && this.flameColor.a > 0.001){
-      g = 0.3;
-      r = 0.06;
-      cr = Mathf.random(0.1);
-
-      Draw.alpha(((1 - g) + Mathf.absin(Time.time(), 8, g) + Mathf.random(r) - r) * entity.warmup);
-
-      Draw.tint(this.flameColor);
-      Fill.circle(tile.drawx(), tile.drawy(), 3 + Mathf.absin(Time.time(), 5, 2) + cr);
-      Draw.rect(this.topRegion, tile.drawx(), tile.drawy());
-      Draw.color(0, 0, 0, entity.warmup);
-      Fill.circle(tile.drawx(), tile.drawy(), 1.9 + Mathf.absin(Time.time(), 5, 1) + cr);
-
-      Draw.color();
-    }
+    this.stats.add(BlockStat.output, this.itemsYeeted[0]);
+    this.stats.add(BlockStat.output, this.itemsYeeted[1]);
   },
   update(tile){
     entity = tile.ent();
@@ -69,5 +43,6 @@ const invertedSiliconSmelter = extendContent(GenericSmelter, "inverted-silicon-s
 });
 invertedSiliconSmelter.craftTime = 40;
 invertedSiliconSmelter.flameColor = Color.valueOf("001066");
-invertedSiliconSmelter.outputItems = [ItemStack(Items.coal, 1), ItemStack(Items.sand, 2)];
+invertedSiliconSmelter.outputItem = ItemStack(Items.coal, 1); //has no functionality, jus tricks the normal java to stop producting.
+invertedSiliconSmelter.itemsYeeted = [ItemStack(Items.coal, 1), ItemStack(Items.sand, 2)];
 invertedSiliconSmelter.craftEffect = Fx.smeltsmoke
