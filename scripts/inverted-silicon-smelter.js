13dea7b4ccd8f7ddf4b7a146509be91adc4db528
MEEP of Faith <meepmatthew@gmail.com>
2020-05-07 18:57:23 -0700

    get the inverted color flame to work

--- scripts/inverted-silicon-smelter.js
+++ scripts/inverted-silicon-smelter.js
@@ -1,10 +1,36 @@
-const invertedSiliconSmelter = extendContent(GenericSmelter, "inverted-silicon-smelter", {
+const invertedSiliconSmelter = extendContent(GenericCrafter, "inverted-silicon-smelter", {
   setStats(){
     this.super$setStats();
     
     this.stats.add(BlockStat.output, this.outputItems[0]);
     this.stats.add(BlockStat.output, this.outputItems[1]);
   },
+  load(){
+    super.load();
+    this.topRegion = Core.atlas.find(name + "-top");
+  },
+  draw(tile){
+    super.draw(tile);
+
+    GenericCrafterEntity entity = tile.ent();
+
+    //draw glowing center
+    if(entity.warmup > 0 && this.flameColor.a > 0.001){
+      g = 0.3;
+      r = 0.06;
+      cr = Mathf.random(0.1);
+
+      Draw.alpha(((1 - g) + Mathf.absin(Time.time(), 8, g) + Mathf.random(r) - r) * entity.warmup);
+
+      Draw.tint(this.flameColor);
+      Fill.circle(tile.drawx(), tile.drawy(), 3 + Mathf.absin(Time.time(), 5, 2) + cr);
+      Draw.rect(this.topRegion, tile.drawx(), tile.drawy());
+      Draw.color(0, 0, 0, entity.warmup);
+      Fill.circle(tile.drawx(), tile.drawy(), 1.9 + Mathf.absin(Time.time(), 5, 1) + cr);
+
+      Draw.color();
+    }
+  }
   update(tile){
     entity = tile.ent();
     
