9b7e86d126e38827b1f33d399c4eff5580847a13
MEEP of Faith <meepmatthew@gmail.com>
2020-05-09 18:57:11 -0700

    Fix Inverted Oil Extractor

--- scripts/inverted-oil-extractor.js
+++ scripts/inverted-oil-extractor.js
@@ -1,19 +1,29 @@
 const invertedOilExtractor = extendContent(GenericCrafter, "inverted-oil-extractor", {
-
+  load(){
+    this.super$load();
+    this.rotatorRegion = Core.atlas.find(this.name + "-rotator");
+    this.topRegion = Core.atlas.find(this.name + "-top");
+    this.liquidRegion = Core.atlas.find(this.name + "-liquid");
+  },
 	draw(tile){
-
-    Draw.rect(this.region, tile.drawx(), tile.drawy());
-
-    Draw.rect(Core.atlas.find(this.name + "-rotator"), tile.drawx(), tile.drawy(), tile.entity.totalProgress * -2);
+    this.super$draw(tile);
+    entity = tile.ent();
+    
+    Draw.color(tile.entity.liquids.current().color);
+    Draw.alpha(tile.entity.liquids.total() / this.liquidCapacity);
+    Draw.rect(this.liquidRegion, tile.drawx(), tile.drawy());
+    
+    Draw.reset();
+    
+    Draw.rect(this.rotatorRegion, tile.drawx(), tile.drawy(), tile.entity.totalProgress * -1);
+    Draw.rect(this.topRegion, tile.drawx(), tile.drawy());
 	},
-
-	generateIcons: function(){
+	generateIcons(){
 		return [
 			Core.atlas.find(this.name),
 			Core.atlas.find(this.name + "-rotator"),
+			Core.atlas.find(this.name + "-top")
 		];
-	},
-	
+	}
 });
-
-invertedOilExtractor.layer = Layer.turret;;
+invertedOilExtractor.liquidCapacity = 30;
